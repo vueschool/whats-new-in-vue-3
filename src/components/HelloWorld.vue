e537fbc8bd20789abcd59460fd50266b433a751b
Alex Kyriakidis <hootlex@icloud.com>
2020-09-16 20:07:15 +0200

    clean up

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,14 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <DataSender
-      @sending-start="started"
-      @sending-complete="completed"
-    />
   </div>
 </template>
 
 <script>
-import DataSender from "@/components/DataSender";
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  },
-  components: {DataSender},
-  methods: {
-    started () {
-      console.log('🟢 Started')
-    },
-    completed () {
-      console.log('🏁 Completed')
-    }
   }
 }
 </script>
